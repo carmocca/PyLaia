04bc75ce84d84a24666702cd0dfc28b808602b58
Carlos Mocholí <carlossmocholi@gmail.com>
2020-08-19 23:58:32 +0200

    WIP - Add probabilities to segmentation

--- laia/utils/segmentation.py
+++ laia/utils/segmentation.py
@@ -1,9 +1,15 @@
 from typing import List, Tuple, Optional
 
+import torch
+
 
 def char_segmentation(
-    txt: str, seg: List[int], height: int, width: Optional[int] = None
-) -> List[Tuple[str, int, int, int, int]]:
+    txt: str,
+    seg: List[int],
+    prob: torch.tensor,
+    height: int,
+    width: Optional[int] = None,
+) -> List[Tuple[str, float, int, int, int, int]]:
     assert len(txt) + 2 == len(seg)
     if width:
         # Scale the width
@@ -16,7 +22,7 @@ def char_segmentation(
     seg[-1] += 1
     return [
         # (value, p1=(x, y), p2=(x, y))
-        (txt[j], seg[j], 1, seg[j + 1] - 1, height)
+        (txt[j], prob[j].item(), seg[j], 1, seg[j + 1] - 1, height)
         for j in range(len(txt))
     ]
 
@@ -27,25 +33,26 @@ def word_segmentation(
     include_spaces: bool = True,
 ) -> List[Tuple[str, int, int, int, int]]:
     pairs = list(zip(segmentation, segmentation[1:]))
-    assert all(s1[3] + 1 == s2[1] for s1, s2 in pairs)
-    assert all(s1[2] == s2[2] for s1, s2 in pairs)
-    assert all(s1[4] == s2[4] for s1, s2 in pairs)
+    assert all(s1[4] + 1 == s2[2] for s1, s2 in pairs)
+    assert all(s1[3] == s2[3] for s1, s2 in pairs)
+    assert all(s1[5] == s2[5] for s1, s2 in pairs)
     out = []
-    w, w_x1, w_x2 = "", None, None
-    for i, (c, x1, y1, x2, y2) in enumerate(segmentation):
+    w, w_p, w_x1, w_x2 = "", 0.0, None, None
+    for i, (c, p, x1, y1, x2, y2) in enumerate(segmentation):
         if c == space:
             if w:
-                out.append((w, w_x1, y1, w_x2, y2))
+                out.append((w, w_p / len(w), w_x1, y1, w_x2, y2))
             if include_spaces:
-                out.append((c, x1, y1, x2, y2))
+                out.append((c, p, x1, y1, x2, y2))
             w = ""
+            w_p = 0.0
             w_x1 = x2 + 1
         else:
             if i == 0:
-
                 w_x1 = x1
             w += c
+            w_p += p
             w_x2 = x2
         if i == len(segmentation) - 1 and w:
-            out.append((w, w_x1, y1, x2, y2))
+            out.append((w, w_p / len(w), w_x1, y1, x2, y2))
     return out
